d04a597a3e7ef316156426ce875721752d8dc769
Hackerboi404 <tuturahul14rk@gmail.com>
2026-05-11 20:40:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,5 @@ class Config:
     API_ID = int(os.getenv("API_ID"))
     API_HASH = os.getenv("API_HASH")
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    
-    # Database Path
     DATABASE_PATH = "database/family.db"
-    
-    # Log Channel (Optional: Put your channel ID here to log errors)
     LOG_CHANNEL = None
