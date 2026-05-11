38ceada8b3ad831cfcfa92e7ec1ee0887dd1b022
Hackerboi404 <tuturahul14rk@gmail.com>
2026-05-11 21:03:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-import asyncio
-from pyrogram import Client, idle
+from threading import Thread
+from pyrogram import Client
+
 from config import Config
 from database.db import init_db
 from handlers import start, family, addrelations
 from live import keep_alive
 
-# Initialize the app
-app = Client("familytreebot", api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN)
 
 print("🌳 Starting FamilyTree Bot...")
 
-# Start Flask Server for Keep-Alive
-keep_alive()
 
-# Initialize Database
+# Start Flask keep alive server
+Thread(target=keep_alive).start()
+
+
+# Initialize database
 init_db()
 
-# Register Dynamic Handlers (Add Relations)
+
+# Create bot client
+app = Client(
+    "familytreebot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN
+)
+
+
+# Register handlers
+start.register_handlers(app)
+family.register_handlers(app)
 addrelations.register_handlers(app)
 
-print("Bot is running in Polling Mode...")
+
+print("✅ Bot is running in Polling Mode...")
+
+
+# Run bot
 app.run()
-idle()
